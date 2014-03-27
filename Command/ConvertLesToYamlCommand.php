d821a8be7dfe3f18a92af6b2a58b65450f489ca4
ahundiak <ahundiak@spike.mshome.net>
2014-03-27 15:34:29 -0500

    Convert Les Schedule to Yaml

---
+++ Command/ConvertLesToYamlCommand.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Cerad\Bundle\GameBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+//  Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+//  Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+use Symfony\Component\Yaml\Yaml;
+
+class ConvertLesToYamlCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('cerad_game__convert__les_to_yaml')
+            ->setDescription('Convert Les Schedule to Yaml')
+        ;
+    }
+    protected function getService($id)     { return $this->getContainer()->get($id); }
+    protected function getParameter($name) { return $this->getContainer()->getParameter($name); }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $loader = $this->getService('cerad_game__convert__les_to_yaml');
+        $games = $loader->load('data/Classic20140327.xlsx');
+        
+        echo sprintf("Games: %d\n",count($games));
+        
+        file_put_contents('data/Classic20140327.yml',Yaml::dump($games,10));
+ 
+        return; if($input); if($output);
+    }
+    protected function processLesSchedule()
+    {
+        $loader = $this->getService('cerad_arbiter.schedule.tourn.les.load');
+        
+        $datax = $this->getParameter('datax');
+        
+        $items = $loader->load($datax . '/ScheduleLes20131014a.xlsx');
+        
+        echo sprintf("Games: %d\n",count($items));
+        
+        $saver = $this->getService('cerad_arbiter.schedule.tourn.arbiter.save');
+        
+        $saver->save($datax . '/ScheduleArbiter20131014a.csv',$items);
+    }
+}
+?>
---
+++ Schedule/Convert/ConvertLesToYaml.php
@@ -0,0 +1,135 @@
+<?php
+namespace Cerad\Bundle\GameBundle\Schedule\Convert;
+
+use Cerad\Bundle\CoreBundle\Excel\Loader as BaseLoader;
+
+class ConvertLesToYaml extends BaseLoader
+{
+    protected $record = array
+    (
+        'num'     => array('cols' => 'Game Number',     'req' => true),
+        'date'    => array('cols' => 'Game Date',       'req' => true),
+        'timeBeg' => array('cols' => 'Game Start Time', 'req' => true),
+        'timeEnd' => array('cols' => 'Game End Time',   'req' => true),
+        
+        'flight'   => array('cols' => 'Flight Name',       'req' => true),
+        'bracket'  => array('cols' => 'Bracket',           'req' => true),
+        'field'    => array('cols' => 'Field Name (full)', 'req' => true),
+        'type'     => array('cols' => 'Match Type',        'req' => true),
+        
+        'homeSeed' => array('cols' => 'Home Seed', 'req' => true),
+        'homeClub' => array('cols' => 'Home Club', 'req' => true),
+        'homeTeam' => array('cols' => 'Home Team Full', 'req' => true),
+        
+        'awaySeed' => array('cols' => 'Away Seed', 'req' => true),
+        'awayClub' => array('cols' => 'Away Club', 'req' => true),
+        'awayTeam' => array('cols' => 'Away Team Full', 'req' => true),
+      
+    );
+    protected function processItem($item)
+    {
+        $num = $item['num'] + 6000;
+        
+        $date    = $this->excel->processDate($item['date']);
+        $timeBeg = $this->excel->processTime($item['timeBeg']);
+        $timeEnd = $this->excel->processTime($item['timeEnd']);
+                
+        $level = $item['flight'];
+        if (substr($level,0,2) == 'U9') $level = 'U09' . substr($level,2);
+        
+        $bracket = $item['bracket'];
+        if (substr($bracket,0,2) == 'U9') $bracket = 'U09' . substr($bracket,2);
+        
+        switch($item['type'])
+        {
+            case 'Group Play':       $type = 'PP'; break;
+            case 'Semi-Finals':      $type = 'SF'; break;
+            case 'Final':            $type = 'FM'; break;
+            case 'Consolation':      $type = 'CM'; break;
+            case 'Consolation Game': $type = 'CM'; break;
+            case 'Quarter-Finals':   $type = 'QF'; break;
+            
+            default: print_r($item); die("\nTYPE\n");
+        }
+        if (strlen($item['homeTeam'])) $homeTeam = $item['homeTeam'];
+        else                           $homeTeam = $type . ' ' . $item['homeClub'];
+        
+        if (strlen($item['awayTeam'])) $awayTeam = $item['awayTeam'];
+        else                           $awayTeam = $type . ' ' . $item['awayClub'];
+        
+        if ($homeTeam[0] == "'") $homeTeam = substr($homeTeam,1);
+        if ($awayTeam[0] == "'") $awayTeam = substr($awayTeam,1);
+        
+        $site = isset($this->sites[$item['field']]) ? $this->sites[$item['field']] : null;
+        if (!$site)
+        {
+            print_r($item); die("\nFIELD\n");
+        }
+        $parts = explode(',',$site);
+        $venueName = trim($parts[0]);
+        $fieldName = trim($parts[1]);
+        
+        $game = array
+        (
+            'num'     => $num,
+            'type'    => 'Game',
+
+            'dtBeg'   => $date . ' ' . $timeBeg,
+            'dtEnd'   => $date . ' ' . $timeEnd,
+
+            'groupKey'  => $bracket,
+            'groupType' => $type,
+            
+            'levelKey'  => $level,
+            
+            'fieldName' => $fieldName,
+            'venueName' => $venueName,
+            
+            'homeTeamName'  => $homeTeam,
+            'awayTeamName'  => $awayTeam,
+            
+            'projectKey' => 'USSF_AL_HFCClassic_Spring2014',
+        );
+        
+        $this->items[] = $game;
+        
+      //echo sprintf("%4d %8s %8s %-16s %-20s %-40s %-40s\n",$num,$date,$time,$level,$site,$homeTeam,$awayTeam);
+        
+      //print_r($game); die("\n");
+    }
+    protected $sites = array
+    (
+        'Merrimack 1a Big North #M01N'   => 'Merrimack, MM01N',  
+        'Merrimack 1b South Small #M01S' => 'Merrimack, MM01S',  
+        
+        'Merrimack 1 South #M01S'        => 'Merrimack, MM01S',
+        'Merrimack 1 North #M01N'        => 'Merrimack, MM01N',
+        
+        'Merrimack 10 Small #M10S'       => 'Merrimack, MM10',  
+        'Merrimack 10 Big #M10B'         => 'Merrimack, MM10',  
+        'Merrimack 10 #M10'              => 'Merrimack, MM10',
+        'Merrimack 9 Big #M09B'          => 'Merrimack, MM09',  
+        
+        'Merrimack 1 #M01'               => 'Merrimack, MM01',  
+        'Merrimack 2 #M02'               => 'Merrimack, MM02',  
+        'Merrimack 3 #M03'               => 'Merrimack, MM03',  
+        'Merrimack 4 #M04'               => 'Merrimack, MM04',  
+        'Merrimack 5 #M05'               => 'Merrimack, MM05',  
+        'Merrimack 6 #M06'               => 'Merrimack, MM06',  
+        'Merrimack 7 #M07'               => 'Merrimack, MM07',  
+        'Merrimack 8 #M08'               => 'Merrimack, MM08',  
+        'Merrimack 9 #M09'               => 'Merrimack, MM09',  
+      
+        'John Hunt 1 #JH1'        => 'John Hunt, JH01',
+        'John Hunt 2 #JH2'        => 'John Hunt, JH02',
+        'John Hunt 3 #JH3'        => 'John Hunt, JH03',
+        'John Hunt 4 #JH4'        => 'John Hunt, JH04',
+        'John Hunt 5 Small #JH5S' => 'John Hunt, JH05',
+        'John Hunt 5 big #JH5B'   => 'John Hunt, JH05',
+        'John Hunt 6 #JH6'        => 'John Hunt, JH06',
+        
+        'City South #CS' => 'City South',
+        'City North #CN' => 'City North',
+    );
+}
+?>
